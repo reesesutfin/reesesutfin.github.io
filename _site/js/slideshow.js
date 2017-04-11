9dec47806089de7a4ee54ffbc21795c29f391ace
Skye Lambert <skyelambert@protonmail.ch>
2017-04-11 14:58:59 -0700

    remove _site

--- _site/js/slideshow.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.fadein img:gt(0)').hide();
-    setInterval(function(){
-      $('.fadein :first-child').fadeOut(750)
-         .next('img').fadeIn(750)
-         .end().appendTo('.fadein');}, 
-      3500);
-});
